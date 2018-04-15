7750feeb53bfdcaef80cf0d312bcca074feaefde
JO <joxphillips@gmail.com>
2018-04-14 22:53:56 -0600

    Equation builder class added

---
+++ MathBattle/src/mathbattle/Equation.java
@@ -0,0 +1,35 @@
+package mathbattle;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Equation
+{
+   
+   private int userFactor; 
+
+   int[] product = new int[13]; 
+   String[] equations = new String[13];
+   Scanner input = new Scanner(System.in);
+   Random rand = new Random();
+
+
+	public int table(int userFactor)
+	{
+	   System.out.print("Enter in a number: ");
+           userFactor = input.nextInt();
+           input.nextLine();
+
+	   for (int i = 0; i < product.length; i++)
+	   {
+		product[i] = i * userFactor;
+   	   }
+
+	   for (int i = 0; i < product.length; i++)
+	   {
+		System.out.printf("%d %c %d %c %d%n", 
+		    userFactor, '\u00D7', i, '\u003D', product[i]);
+		
+	   } 
+	}
+}
+
