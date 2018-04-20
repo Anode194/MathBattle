6b3dc7781f65c1278aec53c1270e007bffe495d4
Anode194 <joxphillips@gmail.com>
2018-04-20 13:51:18 -0600

    Fixed bug that was causing the comparison method and the eq builder method to not work.

--- src/mathbattle/MathBattle.java
+++ src/mathbattle/MathBattle.java
@@ -5,6 +5,7 @@
  */
 package mathbattle;
 
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -19,7 +20,14 @@ public class MathBattle
 	 */
 	public static void main(String[] args)
 	{
-	
+		/*int userFactor = 4;
+		Random rand = new Random();
+	int eqArray[] = new int[3];
+         eqArray[0] = userFactor;
+        eqArray[1] = rand.nextInt(12);
+        eqArray[2] = eqArray[0] * eqArray[1];*/
+
+		//System.out.println(eqArray[2]);
 	int playerNumber =0;
 	Scanner in = new Scanner(System.in);
 	System.out.println("enter 1 for single player mode. Enter 2 for two player mode: ");
@@ -30,7 +38,8 @@ public class MathBattle
 			String playerName = in.next();
 			System.out.println("Please enter the number you would like to study: ");
 			int studyNumber = in.nextInt();
-			MathController mc = new MathController(studyNumber);
+			Equation newEq = new Equation(studyNumber);
+			MathController mc = new MathController(studyNumber, newEq);
 			mc.setCurrentEquation();
 
 		Interface FirstInterface = new Interface(playerNumber,playerName,mc);
